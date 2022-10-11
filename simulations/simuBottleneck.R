953f1b9cc761d4e4976cfcf9fbe90b08d5643ecc
xavierdidelot <xavier.didelot@gmail.com>
2022-10-11 09:35:38 +0100

    Update simuBottleneck.R

--- simulations/simuBottleneck.R
+++ simulations/simuBottleneck.R
@@ -14,22 +14,20 @@ fit=mlskygrid(tree,sampleTimes = sts,res=res,tau=NULL,tau_lower = 0.001,tau_uppe
 fit=parboot(fit,nrep=100,ncpu=6)
 
 pdf('simuBottle.pdf',7,10)
-par(mfrow=c(3,1),mar=c(4,4,1,4))
+par(mfrow=c(2,1),mar=c(4,4,1,4))
 if (!is.null(tree$root.time)) from=tree$root.time else from=-max(dist.nodes(tree)[Ntip(tree)+1,])
 to=from+max(dist.nodes(tree)[Ntip(tree)+1,])
 xs=seq(from=from,to=to,length.out=100)
 ys=xs
 for (i in 1:length(ys)) ys[i]=alphaFun(ys[i])
-plot(xs,ys,type='l',xlab='',ylab='Population size', bty='l',ylim=c(0,1.05*max(ys)))
 plot(tree,show.tip.label = F)
 axisPhylo(1,backward = F)
 plot(fit)
+lines(xs,ys,col='red')
 dev.off()
 system('open simuBottle.pdf')
 
-
-distance=function(alphaFun,fit) {
-  mean((fit$ne-alphaFun(fit$time+max(sampleDates)))^2)
-}
-
-print(distance(alphaFun,fit))
+#distance=function(alphaFun,fit) {
+#  mean((fit$ne-alphaFun(fit$time+max(sampleDates)))^2)
+#}
+#print(distance(alphaFun,fit))
